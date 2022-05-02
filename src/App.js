fb456365f479eae86e6af62ce80f462bdfaa96d2
Marcos8060 <marcosgav80@gmail.com>
2022-05-03 00:19:55 +0300

    created static cart data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CartContainer from './components/CartContainer';
 import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <CartContainer />
     </div>
   );
 }
---
+++ src/components/CartContainer.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import denim from '../images/denim.png'
+import shirt from '../images/shirt.png'
+import shoe from '../images/shoe.png'
+import watch from '../images/watch.png'
+import './css/cartcontainer.css'
+
+
+function CartContainer() {
+  return (
+    <>
+      <div className="app__cartContainer">
+        <div class="container-fluid">
+           <div className="row">
+             <div className="col-md-3">
+               <div className="card">
+                 <img className='img-fluid item_img' src={denim} alt="" />
+                 <div className='d-flex justify-content-between'>
+                    <span>Denim Jacket</span>
+                    <span>$30</span>
+                 </div>
+                 <button className='btn2'>Add to cart</button>
+               </div>
+             </div>
+             <div className="col-md-3">
+               <div className="card">
+                 <img className='img-fluid item_img' src={shirt} alt="" />
+                 <div className='d-flex justify-content-between'>
+                    <span>Denim Jacket</span>
+                    <span>$30</span>
+                 </div>
+                 <button className='btn2'>Add to cart</button>
+               </div>
+             </div>
+             <div className="col-md-3">
+               <div className="card">
+                 <img className='img-fluid item_img' src={shoe} alt="" />
+                 <div className='d-flex justify-content-between'>
+                    <span>Denim Jacket</span>
+                    <span>$30</span>
+                 </div>
+                 <button className='btn2'>Add to cart</button>
+               </div>
+             </div>
+             <div className="col-md-3">
+               <div className="card">
+                 <img className='img-fluid item_img' src={watch} alt="" />
+                 <div className='d-flex justify-content-between'>
+                    <span>Denim Jacket</span>
+                    <span>$30</span>
+                 </div>
+                 <button className='btn2'>Add to cart</button>
+               </div>
+             </div>
+           </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CartContainer
