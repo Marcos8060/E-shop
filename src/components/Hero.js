590b8742967be805c87f385ac6416acf9cc6a3d0
Marcos8060 <marcosgav80@gmail.com>
2022-05-02 22:15:50 +0300

    created the navbar section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import bike from '../images/bike1.png'
+import React from "react";
+import bike from "../images/bike1.png";
+import Navbar from "./Navbar";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <>
+      <div className="app__hero">
+        <Navbar />
+        <div className="container">
+          <div className="col-md-6"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import MenuIcon from '@material-ui/icons/Menu';
+import './css/navbar.css'
+
+
+function Navbar() {
+  return (
+    <>
+     <div className="app__navbar">
+     <nav className="navbar navbar-expand-lg fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+               <span className='logo'>E</span>-shop
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+                <MenuIcon className='menu' />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Categories
+                  </a>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                </ul>
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                  <ShoppingCartIcon />
+                  <span className='quantity'>2</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+     </div>
+    </>
+  )
+}
+
+export default Navbar
