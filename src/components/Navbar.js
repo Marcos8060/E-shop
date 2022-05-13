fafd7f18dbf0b60613f4e88363806506aab4e7e5
Marcos8060 <marcosgav80@gmail.com>
2022-05-13 20:28:59 +0300

    added an onscroll effect in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React,{useEffect,useState,useRef} from "react";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import MenuIcon from "@material-ui/icons/Menu";
 import "./css/navbar.css";
 import {Link} from 'react-router-dom'
 
 function Navbar({ size, setShow}) {
+  const [navBackground, setNavBackground] = useState(false)
+    const navRef = useRef()
+    navRef.current = navBackground
+    useEffect(() => {
+      const handleScroll = () => {
+        const show = window.scrollY > 50
+        if (navRef.current !== show) {
+          setNavBackground(show)
+        }
+      }
+      document.addEventListener('scroll', handleScroll)
+      return () => {
+        document.removeEventListener('scroll', handleScroll)
+      }
+    }, [])
+
   return (
     <>
       <div className="app__navbar">
-        <nav className="navbar navbar-expand-lg fixed-top">
+        <nav className="navbar navbar-expand-lg fixed-top" style={{ transition: '1s ease',backgroundColor: navBackground ? 'black' : 'transparent'}}>
           <div className="container">
             <a className="navbar-brand" onClick={() => setShow(true)}>
               <span  className="logo">E</span>-shop
