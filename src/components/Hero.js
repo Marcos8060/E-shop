e73e2c000cc67ab1b9d1ac3bc99107c6b3b57d2c
Marcos8060 <marcosgav80@gmail.com>
2022-05-13 16:50:25 +0300

    chaged the layout of the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,51 @@
 import React from "react";
 import bike from "../images/bike1.png";
-import Navbar from "./Navbar";
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import PhoneCallbackIcon from '@material-ui/icons/PhoneCallback';
+import HttpsIcon from '@material-ui/icons/Https';
 import "./css/hero.css";
 
 function Hero() {
   return (
     <>
       <div className="app__hero">
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-md-5">
-              <h1 className="intro">50% off for Your First Shopping</h1>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus aliquam porro ratione esse culpa ipsum
-                exercitationem quos laborum. Labore, cumque.
-              </p>
-              <button className="btn1">Shop Now</button>
+              <h1 className="intro">Summer sale <br /> <span className="marketing">30% off.</span></h1>
+              <button className="get">
+                GET YOURS
+              </button>
             </div>
-            <div className="col-md-7">
+            <div className="col-md-7 text-center">
               <img className="img-fluid" src={bike} alt="" />
+            </div>
+          </div>
+          <div className="row g-0">
+            <div className="col-md-3 text-center">
+              <div className="iconItem">
+              <LocalShippingIcon />
+              <span className="m-2">FREE SHIPPING</span>
+              </div>
+            </div>
+            <div className="col-md-3 text-center">
+              <div className="iconItem">
+              <AttachMoneyIcon />
+              <span className="m-2">MONEY BACK</span>
+              </div>
+            </div>
+            <div className="col-md-3 text-center">
+              <div className="iconItem">
+              <PhoneCallbackIcon />
+              <span className="m-2">ONLINE SUPPORT 24/7</span>
+              </div>
+            </div>
+            <div className="col-md-3 text-center">
+              <div className="iconItem">
+                <HttpsIcon />
+                <span className="m-2">SECURE PAYMENT</span>
+              </div>
             </div>
           </div>
         </div>
