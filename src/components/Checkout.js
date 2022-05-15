56c5c43a7575eb46c5f4c3db9f3c01ab8140b6f3
Marcos8060 <marcosgav80@gmail.com>
2022-05-16 02:07:01 +0300

    created the checkout page

---
+++ src/components/Checkout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./checkout.css";
+import { GlobalContext } from "../context";
+
+function Checkout() {
+  return (
+    <div className="app__checkout">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+              <h3>Hellow world</h3>
+          </div>
+          <div className="col-md-6">
+              <h3>Hellow world</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
