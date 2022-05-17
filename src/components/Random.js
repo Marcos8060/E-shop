28fc4107f7dd98a202799982e20e0e9d0109fb57
Marcos8060 <marcosgav80@gmail.com>
2022-05-17 17:23:17 +0300

    made a minor change

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,17 +1,8 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+import React from 'react'
 import './css/random.css'
 
-const url = 'http://127.0.0.1:8000/api/random/'
 function Random() {
-    const [item,setItem] = useState([])
 
-    useEffect(() =>{
-        axios.get(url)
-        .then((res) =>{
-            setItem(res.data)
-        })
-    },[])
   return (
     <div className='app__random'>
         
