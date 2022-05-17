4924fa5e48a9d68700ecf4e66b25a30f8cb6d952
Marcos8060 <marcosgav80@gmail.com>
2022-05-17 18:01:53 +0300

    styled the cto section

--- src/components/Random.js
+++ src/components/Random.js
@@ -5,7 +5,13 @@ function Random() {
 
   return (
     <div className='app__random'>
-        
+        <div className="cto__title">
+          <h1>Off Road Bicycle</h1>
+        </div>
+        <div className='cto__desc'>
+            <p>Here to bring your life style to the next level.</p>
+            <button className='cto__btn'>Shop Now</button>
+        </div>
     </div>
   )
 }
